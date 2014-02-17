57e90ab7c5ef53b1e50034b5f016e62cf899b12a
Allen <a1b2c3d42003hongkong@hotmail.com>
2014-02-18 01:58:58 +0800

    Add communicate structure

---
+++ message.h
@@ -0,0 +1,29 @@
+#ifndef message_h
+#define message_h
+
+typedef struct arg{
+    int nameLen;
+    char *name;    // name != null --> server client list
+    int ip;
+    short port;    // port 0 for client-to-client connection
+    char msg[256]; // name == null --> message
+    ARG *arg;
+} ARG
+
+typedef struct header{
+    char command;
+    int length;
+    ARG *arg;
+} DATA
+
+ARG *newMsg(char *str);
+ARG *newClient(char *name, int ip, short port);
+DATA *newData(char command);
+
+DATA *toArg(char **client, int len); // client == list of clients name, len == array length of client != num of client
+DATA *toMsg(char *str);
+
+char *getData(DATA *data);
+
+#include "message.c"
+#endif
