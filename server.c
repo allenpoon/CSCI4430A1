da5d01ae49721824d7aa6901a998139cccc7a8dd
Ben NG <vodaben@ieee.org>
2014-02-19 00:00:46 +0800

    Compiled server files. Timestamp: 2014-02-19T00:00:30+0800

--- server.c
+++ server.c
@@ -5,9 +5,10 @@
 //#include "structure.h"
 #include "signal.h"
 #include "message.h"
+#include "server.h"
 
 int main(int argc, char **argv){
-    
+    printf("PORT NUMBER: %d\n", PORT);
     
     return 0;
 }
---
+++ server.h
@@ -0,0 +1,2 @@
+// Port number = 12300 + Group Number
+#define PORT 12310
