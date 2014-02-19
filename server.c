e74fe2f99ccb769b2e43b940c66a04ee9013afd8
Ben NG <vodaben@ieee.org>
2014-02-19 16:33:11 +0800

    Compiled all files. Timestamp: 2014-02-19T16:32:33+0800
    Edited some code of server(It is now looping daemon!)

--- server.c
+++ server.c
@@ -19,42 +19,21 @@ typedef struct _ip{
 	unsigned char d;
 }IP;
 
+typedef struct _accepted{
+	int * sd;
+	int * client_sd;
+}ACCEPT_ARG;
+
 char *getClientAddr(struct sockaddr_in * client_addr){
 	int ipadd = client_addr->sin_addr.s_addr;
 	IP *ipaddr_client = (struct _ip *) &ipadd;
 	static char ip[16];
 	sprintf(ip, "%d.%d.%d.%d",ipaddr_client->a, ipaddr_client->b, ipaddr_client->c, ipaddr_client->d);
-	return &ip;
+	return (char *) &ip;
 }
 
-int main(int argc, char **argv){
-    int sd = socket(AF_INET, SOCK_STREAM, 0);
-    int client_sd;
-    int addr_len;
-    int len;
-	struct sockaddr_in server_addr;
-	struct sockaddr_in client_addr;
-	memset(&server_addr,0,sizeof(server_addr));
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	server_addr.sin_port = htons(PORT);
-
-	if(bind(sd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0){
-		printf("Socket cannot bind to a port. %s \nError number: %d\n", strerror(errno), errno);
-	}
-
-	if(listen(sd,1) < 0){
-		printf("Cannot listen to a port. %s \nError number: %d\n", strerror(errno), errno);
-	}
-
-	addr_len = sizeof(client_addr);
-	if((client_sd = (accept(sd, (struct sockaddr *) &client_addr, &addr_len))) < 0){
-		printf("Connection error. Accepting client failed. %s \nError number: %d\n", strerror(errno), errno);
-	}
-
-	printf("Connected:\nClient Address: %s\n", getClientAddr(&client_addr));
-	//getClientAddr(&client_addr);
-
+void *accepted(void *csd){
+	int client_sd = *(int *)csd;
 	while(1){
 		char buff[100];
 		int len;
@@ -72,10 +51,47 @@ int main(int argc, char **argv){
 		printf("RECEIVED INFO: ");
 		if(strlen(buff)!=0)printf("%s\n",buff);
 		if(strcmp("exit",buff)==0){
+			
 			close(client_sd);
 			break;
 		}
 	}
+}
+
+int main(int argc, char **argv){
+    int sd = socket(AF_INET, SOCK_STREAM, 0);
+    int client_sd;
+    int addr_len;
+    int len;
+
+    pthread_t pth;
+
+	struct sockaddr_in server_addr;
+	struct sockaddr_in client_addr;
+	memset(&server_addr,0,sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(PORT);
+
+	if(bind(sd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0){
+		printf("Socket cannot bind to a port. %s \nError number: %d\n", strerror(errno), errno);
+	}
+
+	if(listen(sd,3) < 0){
+		printf("Cannot listen to a port. %s \nError number: %d\n", strerror(errno), errno);
+	}
+
+	addr_len = sizeof(client_addr);
+	while(1){
+		if((client_sd = (accept(sd, (struct sockaddr *) &client_addr, &addr_len))) < 0){
+			printf("Connection error. Accepting client failed. %s \nError number: %d\n", strerror(errno), errno);
+			exit(0);
+		}
+
+		printf("Connected:\nClient Address: %s\n", getClientAddr(&client_addr));
+		pthread_create(&pth, NULL, accepted, (void *) &client_sd);
+	}
+	pthread_join(pth,NULL);
 
 	close(sd);
     return 0;
